49c2ec8239d6dbb35c64154536a4465a6790eca8
jumaniyozov <jumaniyozovse@gmail.com>
2023-02-08 17:49:47 +0500

    handlers updated

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,11 +17,48 @@ func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+	_ = app.Session.RenewToken(r.Context())
 
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	user, err := app.Models.User.GetByEmail(email)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	validPassword, err := user.PasswordMatches(password)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if !validPassword {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	app.Session.Put(r.Context(), "userID", user.ID)
+	app.Session.Put(r.Context(), "user", user)
+
+	app.Session.Put(r.Context(), "flash", "Successful login!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = app.Session.Destroy(r.Context())
+	_ = app.Session.RenewToken(r.Context())
 
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
@@ -29,9 +66,20 @@ func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+	// create a new user
+
+	// send an activation email
+
+	// subscribe the user to an account
 
 }
 
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+
+	// validate url
+
+	// generate an invoice
+
+	// send an email
 
 }
