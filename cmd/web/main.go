3833b0da6b805f2914ac28cf7f029b00973f115e
jumaniyozov <jumaniyozovse@gmail.com>
2023-02-08 15:07:51 +0500

    init

---
+++ cmd/web/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5"
+)
+
+const webPort = "80"
+
+func main() {
+
+	db := initDB()
+	db.Ping()
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("can't connect to database!!!")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready yet...")
+		} else {
+			log.Println("Connected to database!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for 1 second")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/jumaniyozov/gosub
+
+go 1.20
+
+require (
+	github.com/alexedwards/scs/redisstore v0.0.0-20230201054056-1257cb2752e3 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/gomodule/redigo v1.8.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/text v0.3.8 // indirect
+)
